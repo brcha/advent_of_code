c2bf689c73f3a65865dd53450ddfdcf2a6f231bd
Filip Brcic <brcha@yandex.com>
2022-12-05 18:50:11 +0100

    feat: complete day 01, part 02

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,17 +1,38 @@
 use std::io;
 
+fn process_empty_line(current_list: &mut Vec<u64>, max_calories: &mut u64, top_three: &mut Vec<u64>) {
+    let current_calories = current_list.iter().sum();
+    if current_calories > *max_calories {
+        *max_calories = current_calories;
+    }
+    if top_three.len() < 3 {
+        top_three.push(current_calories);
+        top_three.sort();
+    } else {
+        let mut larger = false;
+        for el in top_three.iter() {
+            if current_calories > *el {
+                larger = true;
+            }
+        }
+        if larger {
+            top_three.push(current_calories);
+            top_three.sort();
+            top_three.remove(0);
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let mut max_calories: u64 = 0;
     let mut current_list = Vec::new();
+    let mut top_three = Vec::new();
 
     for line in io::stdin().lines() {
         let input = line.unwrap();
         if input.is_empty() {
             // End of elf
-            let current_calories = current_list.iter().sum();
-            if current_calories > max_calories {
-                max_calories = current_calories;
-            }
+            process_empty_line(&mut current_list, &mut max_calories, &mut top_three);
             current_list.clear();
         } else {
             let current_entry = input.trim().parse().unwrap();
@@ -19,12 +40,13 @@ fn main() -> io::Result<()> {
         }
     }
 
-    let current_calories = current_list.iter().sum();
-    if current_calories > max_calories {
-        max_calories = current_calories;
-    }
+    process_empty_line(&mut current_list, &mut max_calories, &mut top_three);
 
     println!("Maximum calories is {}", max_calories);
+
+    println!("Top three is {:?}", &top_three);
+    let top_three_sum: u64 = top_three.iter().sum();
+    println!("Top three sum is {}", top_three_sum);
 
     Ok(())
 }
