ecbcbb5b5229f6e329d81b3cdd46f7879b775e84
Filip Brcic <brcha@yandex.com>
2022-12-05 20:00:01 +0100

    feat: complete day 02, part 02

--- 2022/day02/src/main.rs
+++ 2022/day02/src/main.rs
@@ -52,6 +52,23 @@ impl From<RPS> for u64 {
     }
 }
 
+#[derive(Eq, Debug, PartialEq)]
+enum ExpectedResult {
+    Lose,
+    Draw,
+    Win,
+}
+
+impl From<ExpectedResult> for u64 {
+    fn from(value: ExpectedResult) -> Self {
+        match value {
+            ExpectedResult::Lose => 0,
+            ExpectedResult::Draw => 3,
+            ExpectedResult::Win => 6,
+        }
+    }
+}
+
 fn calculate_player_value(input: &str) -> RPS {
     match input {
         "X" => RPS::Rock,
@@ -60,11 +77,45 @@ fn calculate_player_value(input: &str) -> RPS {
     }
 }
 
+fn calculate_player_outcome(input: &str) -> ExpectedResult {
+    match input {
+        "X" => ExpectedResult::Lose,
+        "Y" => ExpectedResult::Draw,
+        _   => ExpectedResult::Win,
+    }
+}
+
 fn calculate_opponent_value(input: &str) -> RPS {
     match input {
         "A" => RPS::Rock,
         "B" => RPS::Paper,
         _   => RPS::Scissors,
+    }
+}
+
+fn what_to_play(opponent: &RPS, expected_result: &ExpectedResult) -> RPS {
+    match opponent {
+        RPS::Rock => {
+            match expected_result {
+                ExpectedResult::Lose => RPS::Scissors,
+                ExpectedResult::Draw => RPS::Rock,
+                ExpectedResult::Win => RPS::Paper,
+            }
+        }
+        RPS::Paper => {
+            match expected_result {
+                ExpectedResult::Lose => RPS::Rock,
+                ExpectedResult::Draw => RPS::Paper,
+                ExpectedResult::Win => RPS::Scissors,
+            }
+        }
+        RPS::Scissors => {
+            match expected_result {
+                ExpectedResult::Lose => RPS::Paper,
+                ExpectedResult::Draw => RPS::Scissors,
+                ExpectedResult::Win => RPS::Rock,
+            }
+        }
     }
 }
 
@@ -78,33 +129,42 @@ fn calculate_result(opponent: &RPS, player: &RPS) -> u64 {
     }
 }
 
-fn calculate_score(input: &String) -> u64 {
+fn calculate_score(input: &String) -> (u64, u64) {
     let mut opponent: RPS = RPS::Rock;
     let mut player: RPS = RPS::Rock;
+    let mut expected_result: ExpectedResult = ExpectedResult::Win;
 
     for (idx, val) in input.trim().split_whitespace().enumerate() {
         if idx == 0 {
             opponent = calculate_opponent_value(val);
         } else {
             player = calculate_player_value(val);
+            expected_result = calculate_player_outcome(val);
         }
     }
 
     let result = calculate_result(&opponent, &player);
     let player_score: u64 = player.into();
 
-    player_score + result
+    let player_score_p2: u64 = what_to_play(&opponent, &expected_result).into();
+    let result_p2: u64 = expected_result.into();
+
+    (player_score + result, player_score_p2 + result_p2)
 }
 
 fn main() -> io::Result<()> {
     let mut score: u64 = 0;
+    let mut score_p2: u64 = 0;
 
     for line in io::stdin().lines() {
         let input = line.unwrap();
-        score += calculate_score(&input);
+        let (sc_p1, sc_p2) = calculate_score(&input);
+        score += sc_p1;
+        score_p2 += sc_p2;
     }
 
-    println!("Total score is: {}", score);
+    println!("Total score (part 01) is: {}", score);
+    println!("Total score (part 02) is: {}", score_p2);
 
     Ok(())
 }
