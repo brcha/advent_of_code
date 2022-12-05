7992bed54332eff30a2ba675ca05fe433c9543d8
Filip Brcic <brcha@yandex.com>
2022-12-05 21:13:02 +0100

    feat: complete day 03, part 02

--- 2022/day03/src/main.rs
+++ 2022/day03/src/main.rs
@@ -27,15 +27,30 @@ fn process_line(line: String) -> String {
 
 fn main() {
     let mut total_score = 0;
+    let mut group_vec = Vec::new();
+    let mut group_prio_sum = 0;
+
     for line in io::stdin().lines() {
         let input = line.unwrap();
-        let processed_line = process_line(input);
+        let processed_line = process_line(input.clone());
         let mut current_score = 0;
         for c in processed_line.chars() {
             current_score += char_to_priority(c);
         }
         total_score += current_score;
+
+        group_vec.push(input);
+        if group_vec.len() == 3 {
+            for ch in group_vec[0].chars() {
+                if group_vec[1].contains(ch) && group_vec[2].contains(ch) {
+                    group_prio_sum += char_to_priority(ch);
+                    break;
+                }
+            }
+            group_vec.clear();
+        }
     }
 
     println!("Total score is {}", total_score);
+    println!("Group priority sum is {}", group_prio_sum);
 }
